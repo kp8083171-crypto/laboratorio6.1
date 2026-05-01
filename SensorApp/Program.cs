ad3d771ce728f5d31a42aae6e7fa920bf1713cc6
Kevin <Kp8083171@gmail.com>
2026-04-30 23:15:40 -0500

    v2: Validación implementada

--- SensorApp/Program.cs
+++ SensorApp/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programa
+{
+    static int LIMITE_GLOBAL = 80;
+
+    static bool ValidarTemperatura(double temp)
+    {
+        if (temp < 0)
+        {
+            Console.WriteLine("Error: valor inválido");
+            return false;
+        }
+        return true;
+    }
+
+    static string EvaluarTemperatura(double temp)
+    {
+        if (temp > LIMITE_GLOBAL)
+            return "ALERTA";
+        return "NORMAL";
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese temperatura: ");
+        double temp = double.Parse(Console.ReadLine());
+
+        if (ValidarTemperatura(temp))
+        {
+            string estado = EvaluarTemperatura(temp);
+            Console.WriteLine("Estado: " + estado);
+        }
+    }
+}
